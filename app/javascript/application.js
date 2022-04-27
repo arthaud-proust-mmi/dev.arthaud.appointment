f53e5ef4e564adb9b7271b7f36e3a3f72b208b74
Arthaud proust <proust@arthaud.dev>
2022-04-27 14:00:43 +0200

    ui book meet

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import * as utils from "shims/utils";
 
+window.utils = utils;
